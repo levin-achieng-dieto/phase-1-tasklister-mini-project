9ef27e487a99e532c61d0971fbae439f1b3f12d3
levin-achieng-dieto <levin.dieto@student.moringaschool.com>
2022-08-23 08:57:34 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+
+
+let funct = function(fun1){
+  document.getElementById('new-task-description').innerHTML = document.querySelector('h2').innerHTML
+}
+
+let button = document.querySelector("submit")
+button.addEventListener("click", funct())
